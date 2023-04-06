3b781ccfe54bae24a8b52811a86d33cc554df7be
Vipin <vipiny6768@gmail.com>
2023-04-06 17:56:11 +0530

    Initial commit

---
+++ src/component/Home.js
@@ -0,0 +1,7 @@
+import React from 'react'; 
+import './Home.css';
+function home(){
+
+
+}
+export default home;
---
+++ src/component/Navbar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+function Navbar(){
+return(
+    <div className='main'>
+     
+    <ul>
+       <li>
+        Home
+        </li>
+        <li>
+        Category
+        </li>
+        <li>
+        New Collection
+        </li>
+       
+     </ul>
+     </div>
+     
+)
+
+}
+export default Navbar;
